928efd2f3cca480ff554ce6ca521a6bc2cc7a204
hquan07 <huyquan1607@gmail.com>
2026-02-13 10:42:10 +0700

    Libraries

---
+++ requirements.R
@@ -0,0 +1,24 @@
+# Core data wrangling & visualization
+tidyverse
+lubridate
+
+# Time-series & forecasting
+tsibble
+fable
+forecast
+
+# Spatial analysis
+sf
+tmap
+leaflet
+spatstat
+
+# Network analysis
+igraph
+tidygraph
+ggraph
+
+# Dashboard & reporting
+shiny
+flexdashboard
+rmarkdown
